ed47f81851f45a16452cbd83318ec14aa86b95f1
OLFDB <olf@eisenzelt.de>
2025-05-17 12:09:49 +0200

    headup plugins

--- navit/plugin_def.h
+++ navit/plugin_def.h
@@ -32,3 +32,4 @@ PLUGIN_CATEGORY(vehicle, (struct vehicle_methods *meth, struct callback_list *cb
 PLUGIN_CATEGORY(event, (struct event_methods *meth))
 PLUGIN_CATEGORY(font, (void *meth))
 PLUGIN_CATEGORY(traffic, (struct navit *nav, struct traffic_methods *meth, struct attr **attrs, struct callback_list *cbl))
+PLUGIN_CATEGORY(headup, (struct navit *nav, struct headup_methods *meth, struct callback_list *cbl, struct attr **attrs))
