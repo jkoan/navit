433bc2be61d491b4f13f3b28ea9c4cebb401c5f9
OLFDB <olf@eisenzelt.de>
2025-05-17 11:21:24 +0200

    build warnings

--- navit/support/glib/gatomic.c
+++ navit/support/glib/gatomic.c
@@ -39,6 +39,31 @@
 #include <pthread.h>
 #endif
 
+gint
+g_atomic_int_exchange_and_add (volatile gint *atomic,
+                               gint           val);
+void
+g_atomic_int_add (volatile gint *atomic,
+                  gint           val);
+gboolean
+g_atomic_int_compare_and_exchange (volatile gint *atomic,
+                   gint           oldval,
+                                   gint           newval);
+gboolean
+g_atomic_pointer_compare_and_exchange (volatile gpointer *atomic,
+                       gpointer           oldval,
+                                       gpointer           newval);
+gint
+ (g_atomic_int_get) (volatile gint *atomic);
+void
+(g_atomic_int_set) (volatile gint *atomic,
+                    gint           newval);
+gpointer
+ (g_atomic_pointer_get) (volatile gpointer *atomic);
+void
+(g_atomic_pointer_set) (volatile gpointer *atomic,
+                        gpointer           newval);
+
 #if defined (__GNUC__)
 # if defined (G_ATOMIC_I486)
 /* Adapted from CVS version 1.10 of glibc's sysdeps/i386/i486/bits/atomic.h
