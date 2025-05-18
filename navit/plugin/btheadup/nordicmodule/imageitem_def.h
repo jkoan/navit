e5f59a58a3c3551de0ba125b166d752e96def5a3
OLFDB <olf@eisenzelt.de>
2025-05-18 09:17:24 +0200

    new

---
+++ navit/plugin/btheadup/nordicmodule/imageitem_def.h
@@ -0,0 +1,39 @@
+#ifndef ITEM
+#define ITEM(x) extern ##x;
+#endif
+#ifndef ITEM2
+#define ITEM2(x,y) extern ##y;
+#endif
+
+ITEM2(65969, nav_straight)
+ITEM2(65971, nav_right_1)
+ITEM2(65972, nav_right_2)
+ITEM2(65973, nav_right_3)
+ITEM2(65974, nav_left_1)
+ITEM2(65975, nav_left_2)
+ITEM2(65976, nav_left_3)
+ITEM2(65977, nav_roundabout_r1)
+ITEM2(65978, nav_roundabout_r2)
+ITEM2(65979, nav_roundabout_r3)
+ITEM2(65980, nav_roundabout_r4)
+ITEM2(65981, nav_roundabout_r5)
+ITEM2(65982, nav_roundabout_r6)
+ITEM2(65983, nav_roundabout_r7)
+ITEM2(65984, nav_roundabout_r8)
+ITEM2(65985, nav_roundabout_l1)
+ITEM2(65986, nav_roundabout_l2)
+ITEM2(65987, nav_roundabout_l3)
+ITEM2(65988, nav_roundabout_l4)
+ITEM2(65989, nav_roundabout_l5)
+ITEM2(65990, nav_roundabout_l6)
+ITEM2(65991, nav_roundabout_l7)
+ITEM2(65992, nav_roundabout_l8)
+ITEM2(66018, nav_destination)
+ITEM2(66092, nav_merge_left)
+ITEM2(66093, nav_merge_right)
+ITEM2(66094, nav_turnaround_left)
+ITEM2(66095, nav_turnaround_right)
+ITEM2(66096, nav_exit_left)
+ITEM2(66097, nav_exit_right)
+ITEM2(66098, nav_keep_left)
+ITEM2(66099, nav_keep_right)
