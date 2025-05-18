2a101e4ad8434fb2d1a0d14ac76c74084c40886d
OLFDB <olf@eisenzelt.de>
2025-05-18 09:12:51 +0200

    new

---
+++ navit/speech/iphone/VSSpeechSynthesizerNavit.h
@@ -0,0 +1,24 @@
+#import  <foundation/foundation.h>
+#import <AVFoundation/AVFoundation.h>
+
+@interface VSSpeechSynthesizerNavit : NSObject <AVSpeechSynthesizerDelegate> {
+}
+
+//+ (id)availableLanguageCodes;
++ (BOOL)isSystemSpeaking;
+- (id)startSpeakingString:(id)string;
+- (id)startSpeakingString:(id)string toURL:(id)url;
+- (id)startSpeakingString:(id)string toURL:(id)url withLanguageCode:(id)code;
+- (float)rate;              // default rate: 1
+- (id)setRate:(float)rate;
+- (float)pitch;             // default pitch: 0.5
+- (id)setPitch:(float)pitch;
+- (float)volume;            // default volume: 0.8
+- (id)setVolume:(float)volume;
+- (id)useHFP:(int)use_hfp force:(int)force;  // We want to use HFP to announce, default true
+- (BOOL)is_useHFP;
+- (void)setHFPDelay:(double) delay;
+- (void)setLanguage:(char*) lang;
+- (void)setCountryLanguage:(char *)language;
+- (BOOL)is_speaking;
+@end
